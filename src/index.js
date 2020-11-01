7fb412d8468468d8d0ecf91f0283b2c5eb6ad3ee
solomiya <beshleys16@gmail.com>
2020-11-02 01:52:47 +0200

    add  gates page

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import GuaranteesHeader from './guaranteesHeader'
 import * as serviceWorker from './serviceWorker';
 import AsNavFor from  "./js/slider";
+import GatesWicket from "./js/gatesAndWickets"
 ReactDOM.render(
   <React.StrictMode>
       <AsNavFor />,
-      <GuaranteesHeader />
+      <GuaranteesHeader />,
+      <GatesWicket/>
   </React.StrictMode>,
   document.getElementById('root')
 );
